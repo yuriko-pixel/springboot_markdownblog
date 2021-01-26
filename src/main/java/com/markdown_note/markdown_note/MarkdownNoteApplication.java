4a400fd7039f07b2ed0b4b2cba9586eaa2746992
Yuriko Darragh <beljar731@gmail.com>
2021-01-26 11:23:43 +0900

    first commit

---
+++ src/main/java/com/markdown_note/markdown_note/MarkdownNoteApplication.java
@@ -0,0 +1,13 @@
+package com.markdown_note.markdown_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markdown_note/markdown_note/MarkdownNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markdown_note.markdown_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

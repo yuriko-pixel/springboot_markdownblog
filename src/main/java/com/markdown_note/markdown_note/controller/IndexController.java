5c64d912ed66106096b2efded8251322b7a3a220
Yuriko Darragh <beljar731@gmail.com>
2021-01-26 22:04:34 +0900

    modified block

--- src/main/java/com/markdown_note/markdown_note/controller/IndexController.java
+++ src/main/java/com/markdown_note/markdown_note/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.markdown_note.markdown_note.controller;
 
+import java.util.List;
+
 import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -29,16 +31,20 @@ public class IndexController {
 		post.setHtml(markdownToHTML(post.getContent()));
 		post.setId((long) 1);
 		postService.savePost(post);
-		System.out.println(postService.getAllPosts());
+		List<Post> postList = postService.getAllPosts();
+		for(Post p:postList) {
+			System.out.println(p.getContent().contains("\r"));
+		}
 		model.addAttribute("postList", postService.getAllPosts());
 		return "saved";
 	}
 
 	private String markdownToHTML(String markdown) {
+		String md = markdown.replaceAll("\r","<br/>");
 		Parser parser = Parser.builder().build();
-		Node document = parser.parse(markdown);
+		Node document = parser.parse(md);
+//		HtmlRenderer renderer = HtmlRenderer.builder().softbreak("<br/>").build();
 		HtmlRenderer renderer = HtmlRenderer.builder().build();
-
 		return renderer.render(document);
 	}
 }
